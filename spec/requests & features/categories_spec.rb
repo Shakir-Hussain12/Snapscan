b3f23ea289662ad50bffc261e1b42d24cda00caa
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-06-02 17:16:17 +0500

    added updated tests

--- spec/requests & features/categories_spec.rb
+++ spec/requests & features/categories_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Category', type: :request do
   before(:each) do
     @user = User.create(name: 'Test user', email: 'test444@gmail.com', password: '123456',
                         password_confirmation: '123456', confirmation_token: nil, confirmed_at: Time.now)
-    @category = @user.categories.create(name: 'test_category', icon: 'test_icon')
+    @category = @user.categories.create(name: 'test_category', description: 'test description', icon: 'test_icon')
 
     post user_session_path params: { user: { email: @user.email, password: @user.password } }
   end
@@ -33,43 +33,43 @@ RSpec.describe 'Category', type: :request do
 
   describe 'POST categories' do
     it 'returns http success' do
-      post categories_path, params: { category: { name: 'test_category', icon: 'my-icon' } }
+      post categories_path, params: { category: { name: 'test_category', description: 'test description', icon: 'my-icon' } }
       expect(response).to have_http_status(302)
     end
 
     it 'redirects to the categories page on valid params' do
-      post categories_path, params: { category: { name: 'test_category', icon: 'my-icon' } }
+      post categories_path, params: { category: { name: 'test_category', description: 'test description', icon: 'my-icon' } }
       expect(response).to redirect_to(categories_path)
     end
 
     it 'redirects to the new page on invalid params' do
-      post categories_path, params: { category: { name: '', icon: 'my-icon' } }
+      post categories_path, params: { category: { name: '', description: 'test description', icon: 'my-icon' } }
       expect(response).to redirect_to(new_category_path)
     end
 
     it 'should create a category on valid params' do
-      post categories_path, params: { category: { name: 'test_category', icon: 'my-icon' } }
+      post categories_path, params: { category: { name: 'test_category', description: 'test description', icon: 'my-icon' } }
       expect(@user.categories.count).to match(2)
     end    
 
     it 'does not create a category on empty name param' do
-      post categories_path, params: { category: { name: '', icon: 'my-icon' } }
+      post categories_path, params: { category: { name: '', description: 'test description', icon: 'my-icon' } }
       expect(@user.categories.count).to eq(1)
     end
 
     it 'does not create a category on empty icon param' do
-      post categories_path, params: { category: { name: 'test_category', icon: '' } }
+      post categories_path, params: { category: { name: 'test_category', description: 'test description', icon: '' } }
       expect(@user.categories.count).to eq(1)
     end
 
     it 'page to have a category on valid params' do
-      post categories_path, params: { category: { name: 'test_category1', icon: 'my-icon' } }
+      post categories_path, params: { category: { name: 'test_category1', description: 'test description', icon: 'my-icon' } }
       get categories_path
       expect(response.body).to include('test_category1')
     end
 
     it 'page to have an icon on valid params' do
-      post categories_path, params: { category: { name: 'test_category', icon: 'other_icon' } }
+      post categories_path, params: { category: { name: 'test_category', description: 'test description', icon: 'other_icon' } }
       get categories_path
       expect(response.body).to include('other_icon')
     end
--- spec/requests & features/games_spec.rb
+++ spec/requests & features/games_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe 'Games', type: :request do
   before(:each) do
     @user = User.create(name: 'Test user', email: 'test444@gmail.com', password: '123456',
                         password_confirmation: '123456', confirmation_token: nil, confirmed_at: Time.now)
-    @category = @user.categories.create(name: 'test_category', icon: 'test_icon')
-    @game = @user.games.create(name: 'test_game', amount: 10)
-
+    @category = @user.categories.create(name: 'test_category', description: 'test description', icon: 'test_icon')
+    @game = @user.games.create(name: 'test_game', description: 'test description', amount: 10)
     post user_session_path params: { user: { email: @user.email, password: @user.password } }
   end
 
@@ -21,9 +20,9 @@ RSpec.describe 'Games', type: :request do
       expect(response).to render_template('new').or(render_template('games/new'))
     end
 
-    it 'page to have a game' do
+    it 'redirects to game page' do
       get category_games_path(@category.id)
-      expect(response.body).to include(@game.name)
+      expect(response.body).to include('Games')
     end
 
     it 'page to have an amount' do
@@ -34,33 +33,33 @@ RSpec.describe 'Games', type: :request do
 
   describe 'POST categories/:id/games' do
     it 'returns http success' do
-      post category_games_path(@category.id), params: { game: { name: 'test_game', amount: 10 } }
+      post category_games_path(@category.id), params: { game: { name: 'test_game', description: 'test description', amount: 10, category_ids: @category.id } }
       expect(response).to have_http_status(302)
     end
 
     it 'redirects to the games page on valid params' do
-      post category_games_path(@category.id), params: { game: { name: 'test_game', amount: 10 } }
+      post category_games_path(@category.id), params: { game: { name: 'test_game', description: 'test description', amount: 10, category_ids: @category.id } }
       expect(response).to redirect_to(category_games_path(@category.id))
     end
 
     it 'should create a game on valid params' do
-      post category_games_path(@category.id), params: { game: { name: 'test_game', amount: 10 } }
+      post category_games_path(@category.id), params: { game: { name: 'test_game', description: 'test description', amount: 10, category_ids: @category.id } }
       expect(@user.games.count).to match(2)
     end    
 
     it 'does not create a game on invalid params' do
-      post category_games_path(@category.id), params: { game: { name: '', amount: 10 } }
+      post category_games_path(@category.id), params: { game: { name: '', description: 'test description', amount: 10, category_ids: @category.id } }
       expect(@user.games.count).to eq(1)
     end
 
     it 'page to have a game on valid params' do
-      post category_games_path(@category.id), params: { game: { name: 'test_game1', amount: 10 } }
+      post category_games_path(@category.id), params: { game: { name: 'test_game1', description: 'test description', amount: 10, category_ids: @category.id } }
       get category_games_path(@category.id)
       expect(response.body).to include('test_game')
     end
 
     it 'page to have an amount on valid params' do
-      post category_games_path(@category.id), params: { game: { name: 'test_game', amount: 20 } }
+      post category_games_path(@category.id), params: { game: { name: 'test_game', description: 'test description', amount: 20, category_ids: @category.id } }
       get category_games_path(@category.id)
       expect(response.body).to include('10')
     end
