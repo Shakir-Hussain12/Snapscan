7109476c4046064be581effb8b8d3237ae4da605
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-06-03 05:46:48 +0500

    Fixed Sign-up issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   has_many :categories, dependent: :destroy
   has_many :games, dependent: :destroy
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  if Rails.env.test? || Rails.env.development?
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  else
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  end
+       
   validates :name, presence: true, length: { minimum: 5, maximum: 50 }
 end
